bd5efe605d301940a27bc3a3554a990184f5daa8
Matthew Knight <mattlknight@gmail.com>
2017-01-15 10:38:07 -0600

    Fixes trait SqlSafe to be type independent

--- src/sql/traits/sql_safe.rs
+++ src/sql/traits/sql_safe.rs
@@ -7,7 +7,7 @@ use ::sql::errors::SqlError;
 type Result<T> = result::Result<T, Box<error::Error>>;
 
 pub trait SqlSafe {
-    fn is_sql_safe(&self) -> Result<&str>;
+    fn is_sql_safe(&self) -> Result<&Self>;
 }
 
 lazy_static! {
@@ -15,8 +15,8 @@ lazy_static! {
         Regex::new(r"([^\d\w\s@\.-]+)").expect("FAULT  SQL_CHECK");
 }
 
-impl SqlSafe for str {
-    fn is_sql_safe<'a>(&'a self) -> Result<&'a str> {
+impl<'a> SqlSafe for &'a str {
+    fn is_sql_safe<'b>(&self) -> Result<&Self> {
         if Regex::is_match(&SQL_CHECK, self) {
             let cap = SQL_CHECK.captures(self).expect("FAULT  SQL_CHECK");
             Err(Box::new(SqlError::UnsafeCharacters(cap[1].to_string())))
