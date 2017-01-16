cba8b58e6598cdab62cfb35d9926c86c018ed18e
Matthew Knight <mattlknight@gmail.com>
2017-01-16 14:02:21 -0600

    Adds existing dir check to utility script

--- generate_self_signed_keys.sh
+++ generate_self_signed_keys.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -e "keys" ]
+then
+  echo "keys dir already exists, please delete before re-runninig this script"
+  exit -1
+fi
 mkdir keys
 openssl genrsa -des3 -passout pass:x -out keys/server.pass.key 2048
 openssl rsa -passin pass:x -in keys/server.pass.key -out keys/server.key
@@ -7,6 +12,7 @@ openssl req -new -key keys/server.key -out keys/server.csr
 openssl x509 -req -days 365 -in keys/server.csr -signkey keys/server.key -out keys/server.crt
 ln -s "$(pwd)/keys" "$(pwd)/target/release/"
 ln -s "$(pwd)/keys" "$(pwd)/target/debug/"
+echo "\n\n\n"
 echo "On linux, to support self signed cert in chrome and postman, you must import the cert to chrome"
 echo "in chrome, go to \"chrome://settings/certificates \""
 echo "Click Manage Certificates, on the authorities tab, check for an existing folder/cert for"
